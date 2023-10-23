4ef424b0bd10c94aceb4fd5bdd99a106c2b620ea
jon-torres <jonathantorres10@gmail.com>
2023-10-23 10:31:03 -0300

    refactored code

--- text_generator.py
+++ text_generator.py
@@ -1,115 +1,111 @@
 import os
-import sys
 import re
 import random
 from collections import defaultdict, Counter
+from typing import List, Tuple
 import spacy
-from spacy.tokenizer import Tokenizer
-from typing import List
+from cache_manager import CacheManager
 
-# Global variables. Some can be changed to match what you want to accomplish.
-# Cache for file data
-CACHE = {}
-# Discord's output characters limit.
+# Global constants.
 MAX_SENTENCE_LENGTH = 1999
-START_TOKENS_REGEX = re.compile(r'\S+').match
-CORPUS_DIR = os.path.join(sys.path[0], "corpus")
-# N-grams being used in the model are at least 5-grams (sequences of 5 consecutive words).
-N_GRAM_SIZE = 5
+START_TOKENS_REGEX = re.compile(r"\S+").match
+CORPUS_DIR = os.path.join(os.path.dirname(__file__), "corpus")
+N_GRAM_SIZE = 3
+FINAL_PUNCT = ".!?"
+
+# Initialize Spacy tokenizer and CacheManager.
+nlp = spacy.blank("pt")
+tokenizer = spacy.tokenizer.Tokenizer(
+    nlp.vocab, token_match=START_TOKENS_REGEX
+)
+nlp.tokenizer = tokenizer
+nlp.max_length = 10000000
+cache_manager = (
+    CacheManager()
+)  # Set the desired max cache size, default value is 10
 
 
 def read_file(filename: str) -> str:
-    """Reads the contents of a file located in the specified directory and
-     returns the contents of the file as a string."""
-    with open(os.path.join(CORPUS_DIR, f"{filename}.txt"), encoding="utf-8") as file:
-        text = file.read()
-    return text
+    """Reads and returns content of a file."""
+    with open(
+        os.path.join(CORPUS_DIR, f"{filename}.txt"), encoding="utf-8"
+    ) as file:
+        return file.read()
 
 
-def tokenize_corpus(text: str) -> list:
-    """Tokenizes a given text (string) and returns a list of tokens."""
-    # You can use spacy.load() instead for better accuracy, but it demands more time and resources.
-    nlp = spacy.blank("pt")
-    # This expands Spacy's max length so you can avoid errors.
-    nlp.max_length = len(text) + 100
-    # The modified tokenizer matches any non-whitespace characters.
-    nlp.tokenizer = Tokenizer(nlp.vocab, token_match=START_TOKENS_REGEX)
-    doc = nlp(text)
-    tokens_list = [token.text for token in doc]
-    return tokens_list
+def tokenize_corpus(text: str) -> List:
+    """Tokenizes the given text using Spacy and returns a list of tokens."""
+    return [token.text for token in nlp(text)]
 
 
-def get_ngrams(tokens_list: list) -> defaultdict:
-    """Returns a dictionary with bigrams as keys and a Counter object as values.
-    The Counter object contains the frequency
-    of the next word after each bigram in the input list of tokens."""
+def get_ngrams(tokens_list: list, n=N_GRAM_SIZE) -> defaultdict:
+    """Generates and returns n-grams from a list of tokens."""
     ngrams = defaultdict(Counter)
-    for i in range(len(tokens_list) - 2):
-        bigram = " ".join(tokens_list[i: i + 2])
-        next_word = tokens_list[i + 2]
-        ngrams[bigram].update((next_word,))
+    for i in range(len(tokens_list) - n + 1):
+        ngram = tuple(tokens_list[i : i + n - 1])
+        next_word = tokens_list[i + n - 1]
+        ngrams[ngram].update([next_word])
     return ngrams
 
 
-def preprocess_ngrams(ngrams: defaultdict) -> List[str]:
+def is_valid_start(ngram: tuple) -> bool:
     """
-    Returns a list of word pairs that can start a sentence based on a dictionary of n-grams."""
-    sentence_starts = []
-    for pair in ngrams:
-        if pair[0].isupper() and not pair.endswith((".", "!", "?")):
-            sentence_starts.append(pair)
-    return sentence_starts
-
-
-def select_start(ngrams: defaultdict) -> list:
+    Checks if a given n-gram can be used as a valid starting sequence.
     """
-    Selects a random pair of words from the ngrams dictionary.
+    return ngram[0][0].isupper() and ngram[-1][-1] not in FINAL_PUNCT
+
+
+def select_start(ngrams: defaultdict) -> Tuple:
     """
-    candidate_sentence_starts = [
-        pair.split()
-        for pair in ngrams
-        if pair[0].isupper()
-        and not pair.endswith((".", "!", "?"))
-    ]
-    return random.choice(candidate_sentence_starts)
+    Selects a random valid starting n-gram from the n-grams list.
+    """
+    starts = [ngram for ngram in ngrams if is_valid_start(ngram)]
+    return random.choice(starts) if starts else ()
 
 
-def generate_sentence(ngrams: defaultdict, n: int) -> str:
-    """Generates a new sentence based on the n-gram model represented by the ngrams dictionary."""
-    result = []
-    sentence_starts = preprocess_ngrams(ngrams)
-    while True:
-        if not result:
-            result = select_start(sentence_starts)
-        if len(result) < 2:
-            result.clear()
-            continue
-        prev_word1, prev_word2 = result[-2:]
-        next_word_choices = list(ngrams[f"{prev_word1} {prev_word2}"].keys())
-        if not next_word_choices:
-            result.clear()
-            continue
-        next_word = random.choice(next_word_choices)
-        result.append(next_word)
+def generate_sentence(
+    ngrams: defaultdict, n=N_GRAM_SIZE, max_words=100
+) -> str:
+    """
+    Generates a random sentence using the n-grams.
+    """
+    result = list(select_start(ngrams))
+    if not result:
+        return ""
 
-        if next_word[-1] in ".!?":
-            if len(result) < n:
-                result.clear()
-                continue
-            return " ".join(result)
+    # Ensure the generation continues until a punctuation mark AND within word limit
+    while len(result) < n or (
+        result[-1][-1] not in FINAL_PUNCT and len(result) < max_words
+    ):
+        next_ngram = tuple(result[-(n - 1) :])
+        next_word_choices = list(ngrams[next_ngram].keys())
+        weights = list(
+            ngrams[next_ngram].values()
+        )  # Get the frequencies as weights
+
+        if not next_word_choices or len(result) >= max_words:
+            break
+        # Choose the next word based on its frequency
+        result.append(random.choices(next_word_choices, weights=weights)[0])
+
+    # If the text exceeded the word limit without finding punctuation, trim to the last punctuation
+    if len(result) >= max_words:
+        for i in reversed(range(max_words)):
+            if result[i][-1] in FINAL_PUNCT:
+                result = result[: i + 1]
+                break
+
+    return " ".join(result)
 
 
 def generate_text(filename: str) -> str:
-    """Reads a text file, tokenizes it, generates an n-gram model,
-    and uses it to create a new sentence."""
-    if filename in CACHE:
-        tokenized_text, ngrams = CACHE[filename]
+    data = cache_manager.get(filename)
+    if data:
+        tokenized_text, ngrams = data
     else:
         text = read_file(filename)
         tokenized_text = tokenize_corpus(text)
         ngrams = get_ngrams(tokenized_text)
-        CACHE[filename] = (tokenized_text, ngrams)
-
-    text = generate_sentence(ngrams, N_GRAM_SIZE)
-    text = text[:MAX_SENTENCE_LENGTH]
-    return text
+        cache_manager.set(filename, (tokenized_text, ngrams))
+    text = generate_sentence(ngrams)
+    return text[:MAX_SENTENCE_LENGTH]
